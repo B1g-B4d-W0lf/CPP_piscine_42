9064320518bcfc495bdd299622fffd972c335775
Big Bad Wolf <wfreulon@student.42.fr>
2024-03-05 22:30:16 +0100

    02 push

--- Module_02/ex03/main.cpp
+++ Module_02/ex03/main.cpp
@@ -6,12 +6,15 @@ int main()
 	Point	a(0, 0);
 	Point	b(10, 0);
 	Point	c(0, 10);
-	Point	point(9.4, 0.6);
+	Point	point(9.4, 0.5);
 	bool	res;
 
 	
 	res = bsp(a, b, c, point);
-	std::cout << res << std::endl;
+	if (res)
+		std::cout << "This point is in the triangle" << std::endl;
+	else 
+		std::cout << "This point is.. NOT! In the triangle"<< std::endl;
 
 	return 0;
 }
